553af8f8715860f51e4451a06ac5396ab85303ee
Velmurugan Jeyakumar <velmuruganjeyakumar2002@gmail.com>
2023-05-19 02:19:30 +0530

    added comments for better understanding of app run

--- app.py
+++ app.py
@@ -24,9 +24,14 @@ def search():
 
     top_articles = [articles[sim[0]] for sim in sims[:10]]
 
-
     return render_template('result.html', articles=top_articles)
 
 
 if __name__ == '__main__':
-    app.run(debug=False,port=8080)
+    
+    #  Production
+    from waitress import serve
+    serve(app, host="0.0.0.0", port=8080)
+
+    # local run
+    # app.run(debug=True,port=5000)
